b238ec62d57195d3af39dff67f66977ad9a5bdb6
Saoa35 <saoamah@gmail.com>
2023-08-06 13:51:21 +0400

    + created PostView component

--- App.js
+++ App.js
@@ -1,32 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View, Image } from "react-native";
-import styled from "styled-components/native";
-
-const Post = styled.View`
-  padding: 15px;
-  margin-top: 22px;
-  border-bottom-width: 1px;
-  border-bottom-color: rgba(0, 0, 0, 0.1);
-  border-bottom-style: solid;
-`;
-
-const PostImage = styled.Image`
-  width: 60px;
-  height: 60px;
-  border-radius: 12px;
-  margin-right: 12px;
-`;
+import { View, StatusBar } from "react-native";
+import { Post } from "./src/components/Post";
 
 export default function App() {
   return (
     <View>
-      <Post>
-        <PostImage
-          source={{
-            uri: "https://pbs.twimg.com/media/Eux9UwbVIAIlWBb?format=jpg&name=900x900",
-          }}
-        />
-      </Post>
+      <Post
+        imageUrl={
+          "https://pbs.twimg.com/media/Eux9UwbVIAIlWBb?format=jpg&name=900x900"
+        }
+        title={"Test article"}
+        createDate={"06/08/2023"}
+      />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Post.jsx
@@ -0,0 +1,48 @@
+import { Text, View, Image } from "react-native";
+import styled from "styled-components/native";
+
+const PostView = styled.View`
+  flex-direction: row;
+  padding: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: rgba(0, 0, 0, 0.1);
+  border-bottom-style: solid;
+`;
+
+const PostImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 12px;
+  margin-right: 12px;
+`;
+
+const PostTitle = styled.Text`
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const PostDate = styled.Text`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.4);
+  margin-top: 2px;
+`;
+
+const PostDetails = styled.View`
+  justify-content: center;
+`;
+
+export const Post = ({ imageUrl, title, createDate }) => {
+  return (
+    <PostView>
+      <PostImage
+        source={{
+          uri: imageUrl,
+        }}
+      />
+      <PostDetails>
+        <PostTitle>{title}</PostTitle>
+        <PostDate>{createDate}</PostDate>
+      </PostDetails>
+    </PostView>
+  );
+};
