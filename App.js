28938f5547c4161defac5f157a2b8548994d825c
Saoa35 <saoamah@gmail.com>
2023-08-06 10:40:32 +0400

    + installed styled components

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={{ color: "blue" }}>News App !!!</Text>
       <StatusBar style="auto" />
     </View>
   );
